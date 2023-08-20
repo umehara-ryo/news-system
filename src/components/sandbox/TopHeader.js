6cbb8e6639ae7d64c3ae0927d0f9c7a101b60537
umehara <a603751058@gmail.com>
2023-08-20 11:20:17 +0800

    TopHeaderの編集　完成

--- src/components/sandbox/TopHeader.js
+++ src/components/sandbox/TopHeader.js
@@ -1,10 +1,13 @@
-import {Button, Layout, Menu, theme} from 'antd';
+import {Button, Layout, Menu,Space, theme} from 'antd';
 import {
     MenuFoldOutlined,
-    MenuUnfoldOutlined
+    MenuUnfoldOutlined, UserOutlined
 } from '@ant-design/icons';
 import {useState} from "react";
-
+import Dropdown from "antd/es/dropdown/dropdown";
+import type { MenuProps } from 'antd';
+import { DownOutlined, SmileOutlined } from '@ant-design/icons';
+import Avatar from "antd/es/avatar/avatar";
 
 
 export default function TopHeader(){
@@ -15,6 +18,40 @@ export default function TopHeader(){
     const {
         token: { colorBgContainer },
     } = theme.useToken();
+
+    const items = [
+        {
+            key: '1',
+            label: (
+                <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+                    1st menu item
+                </a>
+            ),
+        },
+        {
+            key: '2',
+            label: (
+                <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+                    2nd menu item
+                </a>
+            ),
+            icon: <SmileOutlined />
+        },
+        {
+            key: '3',
+            label: (
+                <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
+                    3rd menu item
+                </a>
+            ),
+            disabled: false
+        },
+        {
+            key: '4',
+            danger: true,
+            label: '戻る',
+        },
+    ];
 
     return(
         <div>
@@ -34,7 +71,21 @@ export default function TopHeader(){
                         height: 64,
                     }}
                 />
+
+                <div style={{float:"right"}}>
+                    <span>adminようこそ</span>
+
+                    <Dropdown menu={{ items }}>
+                        <a onClick={(e) => e.preventDefault()}>
+                            <Space>
+                                <Avatar size={30} icon={<UserOutlined />} />
+                                <DownOutlined></DownOutlined>
+                            </Space>
+                        </a>
+                    </Dropdown>
+                </div>
             </Header>
+
         </div>
     )
 }
