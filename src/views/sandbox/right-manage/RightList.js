578759e277b2bf2865256afa25022dff889ad140
umehara <a603751058@gmail.com>
2023-08-20 17:11:49 +0800

    権限リストページの雛形　完成

--- src/views/sandbox/right-manage/RightList.js
+++ src/views/sandbox/right-manage/RightList.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {Table} from "antd";
+import axios from "axios";
 
 export default function RightList() {
+
+    const [dataSource,setDataSourse] = useState( [])
+    useEffect(()=>{
+            axios.get('http://localhost:5000/rights').then(res=>{
+                setDataSourse(res.data)
+            })
+    },[])
+
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+        },
+        {
+            title: '権限名前',
+            dataIndex: 'title',
+        },
+        {
+            title: '権限パス',
+            dataIndex: 'key',
+        },
+    ];
+
     return (
         <div>
-            RightList
+
+            <Table dataSource={dataSource} columns={columns}/>;
         </div>
     )
 }
