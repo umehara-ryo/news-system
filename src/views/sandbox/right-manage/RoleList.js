2c61704dac08acbd5b0af3d88d1319ecbc40b4ad
umehara <a603751058@gmail.com>
2023-08-21 13:43:46 +0800

    ロールリストページ骨組み構築　完成 1.<Table>タグを利用して

--- src/views/sandbox/right-manage/RoleList.js
+++ src/views/sandbox/right-manage/RoleList.js
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {Button, Modal, Popover, Switch, Table} from "antd";
+import axios from "axios";
+import {DeleteOutlined, EditOutlined, ExclamationCircleOutlined} from "@ant-design/icons";
 
 export default function RoleList() {
+
+   const [dataSource,setDataSource] = useState([]);
+   const columns = [
+       {
+           title: 'ID',
+           dataIndex: 'id',
+           render: (id) => {
+               return <b>{id}</b>
+           }
+       },
+       {
+           title: 'ロール名',
+           dataIndex: 'roleName',
+           render: (roleName)=>{
+               return<i>{roleName}</i>
+           }
+   },
+       {
+           title: '操作',
+           render: (item)=>{
+               return　<div>
+
+
+                       <Button type="primary" shape="circle" icon={<EditOutlined/>}
+                       ></Button>
+
+                   <Button danger shape="circle" icon={<DeleteOutlined/>}  onClick={()=>confirmDelete(item)}></Button>
+               </div>
+           }
+       },
+
+   ];
+
+    useEffect(()=>{
+        axios.get('http://localhost:5000/roles').then(res=>{
+            console.log(res.data);
+            setDataSource(res.data);
+        })
+
+    },[]);
+
+
+    const {confirm} = Modal;
+    const confirmDelete = (item)=>{
+        confirm({
+            title: 'Confirm',
+            icon: <ExclamationCircleOutlined />,
+            content: '削除しますか',
+            okText: '削除',
+            cancelText: 'キャンセル',
+            onOk() {
+                console.log('OK');
+                deleteMethod(item);
+            },
+            onCancel() {
+                console.log('Cancel');
+            }
+        });
+    }
+
+    const deleteMethod = (item)=>{
+
+            console.log(item);
+            setDataSource(dataSource.filter(data => data.id !== item.id));
+            axios.delete(`http://localhost:5000/roles/${item.id}`);
+
+    }
+
     return (
         <div>
-            RoleList
+               <Table dataSource={dataSource} columns={columns} rowKey={(item)=>item.id}></Table>
         </div>
     )
 }
