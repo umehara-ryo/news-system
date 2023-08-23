0012ac26e82618ea1453f172d049e3d09dae37e7
umehara <a603751058@gmail.com>
2023-08-23 13:09:15 +0800

    ルーティングの動的パス　完成 1.Promise.allによって、同期化 2.exactによって、的確にマッチングする 3.<Route>の下にpathなしの<Route>を作成すると、深刻な問題が発生

--- src/components/sandbox/NewsRouter.js
+++ src/components/sandbox/NewsRouter.js
@@ -12,34 +12,80 @@ import AuditList from "../../views/sandbox/audit-manage/AuditList";
 import Unpublished from "../../views/sandbox/publish-manage/Unpublished";
 import Published from "../../views/sandbox/publish-manage/Published";
 import Sunset from "../../views/sandbox/publish-manage/Sunset";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+
+const localRouterListMap = {
+    "/home":Home,
+    "/user-manage/list":UserList,
+    "/right-manage/role/list":RoleList,
+    "/right-manage/right/list":RightList,
+    "/news-manage/add":NewsAdd,
+    "/news-manage/draft":NewsDraft,
+    "/news-manage/category":NewsCategory,
+    "/audit-manage/audit":Audit,
+    "/audit-manage/list":AuditList,
+    "/publish-manage/unpublished":Unpublished,
+    "/publish-manage/published":Published,
+    "/publish-manage/sunset":Sunset
+}
+
 
 export default function NewsRouter(){
 
-    const localRouterListMap = {
-        "/home":Home,
-        "/user-manage/list":UserList,
-        "/right-manage/role/list":RoleList,
-        "/right-manage/right/list":RightList,
-        "/news-manage/add":NewsAdd,
-        "/news-manage/draft":NewsDraft,
-        "/news-manage/category":NewsCategory,
-        "/audit-manage/audit":Audit,
-        "/audit-manage/list":AuditList,
-        "/publish-manage/unpublished":Unpublished,
-        "/publish-manage/published":Published,
-        "/publish-manage/sunset":Sunset
+    const [BackRouteList,setBackRouteList] = useState([]);
+    useEffect(()=>{
+        Promise.all([
+            axios.get(`http://localhost:5000/rights`),
+            axios.get(`http://localhost:5000/children`),
+        ]).then(res=>{
+            //console.log(res[0].data);
+            //console.log(res[1].data);
+            setBackRouteList([...res[0].data,...res[1].data]);
+        })
+    },[])
+
+    const checkRoute = (item) => {
+
+        //console.log(localRouterListMap[item.key]);
+
+            return localRouterListMap[item.key] && (item.pagepermisson || item.routepermisson);
     }
 
+    const {role:{rights}} = JSON.parse(localStorage.getItem('token'));
+
+    const checkPermission = (item) => {
+        if(rights.includes(item.key)){
+            return true;
+        }
+        return false;
+    }
+
+
     return(
-
-
             <Switch>
-                <Route path='/home' component={Home}></Route>
-                <Route path='/user-manage/list' component={UserList}></Route>
-                <Route path='/right-manage/role/list' component={RoleList}></Route>
-                <Route path='/right-manage/right/list' component={RightList}></Route>
+                {/*<Route path='/home' component={Home}></Route>*/}
+                {/*<Route path='/user-manage/list' component={UserList}></Route>*/}
+                {/*<Route path='/right-manage/role/list' component={RoleList}></Route>*/}
+                {/*<Route path='/right-manage/right/list' component={RightList}></Route>*/}
+
+                {
+                    BackRouteList.map(item=>{
+                        if(checkRoute(item) && checkPermission(item)){
+                           return <Route path={item.key} key={item.key} component={localRouterListMap[item.key]} exact></Route>
+                        }else {
+                            return null;
+                        }
+                        }
+
+                    )
+                }
+
                 <Redirect from='/' to='/home' exact></Redirect>
-                <Route path="*" component={NoPermission}></Route>
+                {
+                    BackRouteList.length>0 && <Route path="*" component={NoPermission}></Route>
+                }
             </Switch>
 
 
