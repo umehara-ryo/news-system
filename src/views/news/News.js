4038498f21fe6b4b509638e01793fba2bf13ee20
umehara <a603751058@gmail.com>
2023-08-26 15:38:57 +0800

    ゲストニュースページ開発　完成 1.cardタグとlistタグの利用

--- src/views/news/News.js
+++ src/views/news/News.js
@@ -1,7 +1,49 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Header} from "antd/es/layout/layout";
+import {Card, Col, List, Row} from "antd";
+import _ from 'lodash';
 export default function News(){
+
+const [list,setList] = useState([]);
+    useEffect(()=>{
+        axios.get('/news?publishState=2&_expand=category').then(res=>{
+            setList(Object.entries(_.groupBy(res.data,item=>item.category.title)));
+            //map To Array
+        })
+    },[])
+
     return(
-        <div>
-            News
+        <div style={{
+            width:'100%',
+            margin:'0 auto'
+        }}>
+           <Header>
+               <b style={{
+                   color:'white',
+                   fontSize:'20px'
+               }}>トップニュース</b>
+           </Header>
+            <Row gutter={[16,16]}>
+                {
+                    list.map((item)=> {
+                        return <Col span={8} key={item[0]}>
+                            <Card title={item[0]} bordered={false} hoverable={true}>
+                                <List
+                                    size="small"
+                                    bordered
+                                    dataSource={item[1]}
+                                    pagination={{
+                                        pageSize: 2
+                                    }}
+                                    renderItem={(data) => <List.Item><a href={`#/detail/${data.id}`}>{data.title}</a></List.Item>}
+                                />
+                            </Card>
+                        </Col>
+                    })
+                }
+
+            </Row>
         </div>
     )
 }
